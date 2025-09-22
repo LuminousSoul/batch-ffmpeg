04c158966f59b03076737b0d55dfd495ee55cea9
Victor <34597561+LuminousSoul@users.noreply.github.com>
2025-09-22 15:49:22 -0600

    Add files via upload

---
+++ batch-add-thumbnail-mp4.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Count total MP4 files first
+set count=0
+for %%f in (*.mp4) do (
+    set /a count+=1
+)
+
+if !count! equ 0 (
+    echo No MP4 files found in this folder.
+    pause
+    exit /b
+)
+
+REM Initialize file index
+set index=0
+
+REM Loop through all MP4 files
+for %%f in (*.mp4) do (
+    set /a index+=1
+    echo Processing file !index! of !count!: "%%f"
+	
+	
+	REM extract Thumbnail
+	ffmpeg -ss 00:00:00 -i "%%f" -frames:v 1 -q:v 2 thumbnail.png
+	
+	ffmpeg -i "%%~nf" -i thumbnail.png -map 1 -map 0 -c copy -disposition:0 attached_pic "%%~nf [T].mp4"
+	del thumbnail.png
+)
+
+echo Conversion complete!
+pause
---
+++ batch-mp4-to-mp4-x265.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Count total MP4 files first
+set count=0
+for %%f in (*.mp4) do (
+    set /a count+=1
+)
+
+if !count! equ 0 (
+    echo No MP4 files found in this folder.
+    pause
+    exit /b
+)
+
+REM Initialize file index
+set index=0
+
+REM Loop through all MP4 files
+for %%f in (*.mp4) do (
+    set /a index+=1
+    echo Processing file !index! of !count!: "%%f"
+	
+	
+	REM extract Thumbnail
+	ffmpeg -ss 00:00:00 -i "%%f" -frames:v 1 -q:v 2 thumbnail.png
+	
+
+	:: Other Commands to add if needed
+	:: Scaling down - -vf "scale='min(1920,iw)':'min(1080,ih)'" ^
+    ffmpeg -i "%%f" -c:v libx265 -preset ultrafast -crf 28 ^
+        -c:a aac -b:a 192k ^
+        -c:s copy ^
+        "%%~nf [x265] [No Thumbnail].mp4"
+		
+		
+		ffmpeg -i "%%~nf [x265] [No Thumbnail].mp4" -i thumbnail.png -map 1 -map 0 -c copy -disposition:0 attached_pic "%%~nf [x265].mp4"
+		del "%%~nf [x265] [No Thumbnail].mp4"
+		del thumbnail.png
+)
+
+echo Conversion complete!
+pause
